bde66674868edcb00d77049cf43662b112775e27
Ryan Mwangi <ryannganga13325@gmail.com>
2026-05-23 01:02:58 +0300

    Updates

---
+++ client/assets/marketing-creative-DU8Ldeky.js
@@ -0,0 +1,1 @@
+const a="/mindscape-canvas-58/assets/marketing-creative-yxJufrG3.png";export{a as m};
---
+++ server/assets/marketing-creative-DsJtwj1w.js
@@ -0,0 +1,4 @@
+const marketingCreative = "/mindscape-canvas-58/assets/marketing-creative-yxJufrG3.png";
+export {
+  marketingCreative as m
+};
